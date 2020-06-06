7528db80cd434fbc503db54ee2099663041c4965
Kasper Munch Terkelsen <kmt@birc.au.dk>
2020-06-06 21:40:20 +0200

    Added cache

--- geneinfo/__init__.py
+++ geneinfo/__init__.py
@@ -102,16 +102,28 @@ def plot_gene(name, txstart, txend, strand, exons, gene_type, offset, ax):
         line = ax.plot([start, end], [offset, offset], linewidth=10, color=color)
         line[0].set_solid_capstyle('butt')
         
-    ax.text(txstart, offset+.5, name, horizontalalignment='right', verticalalignment='center', fontsize=10)#, transform=ax.transAxes)
+    ax.text(txstart, offset+.5, name, horizontalalignment='right', verticalalignment='center', fontsize=8)#, transform=ax.transAxes)
 
+
+CACHE = dict()
 
 def geneplot(chrom, start, end, hg19=False, figsize=None):
     "Specifying hg19 gives gene coordintes in hg19, but give chrom, start and end are still assumed to be 38"
     
+    global CACHE
+
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=figsize, sharex='col', sharey='row')
     plt.subplots_adjust(wspace=0, hspace=0)
 
-    for offset, (name, txstart, txend, strand, exons, gene_type) in enumerate(get_genes(chrom, start, end, hg19=hg19)):
+    if (chrom, start, end, hg19) in CACHE:
+        genes = CACHE[(chrom, start, end, hg19)]
+    else:
+        genes = list(get_genes(chrom, start, end, hg19=hg19))
+        CACHE[(chrom, start, end, hg19)] = genes
+
+    offset = 0 # incase of no genes
+
+    for offset, (name, txstart, txend, strand, exons, gene_type) in enumerate(genes):
         plot_gene(name, txstart, txend, strand, exons, gene_type, offset, ax=ax2)
 
     ax2.get_yaxis().set_visible(False)
@@ -126,3 +138,20 @@ def geneinfo_region(chrom, start, end, hg19=False):
     
     for gene in mg.query(f'q={chrom}:{start}-{end}', species='human', fetch_all=True):
         geneinfo(gene['symbol'])
+
+
+if __name__ == "__main__":
+
+    import mygene
+
+    mg = mygene.MyGeneInfo()
+    connect_mygene(mg)
+
+    chrom, start, end = 'chr3', 49500000, 50600000
+    ax = geneplot(chrom, start, end, figsize=(10, 5))
+    ax.plot(np.linspace(start, end, 1000), np.random.random(1000), 'o')
+    plt.savefig('tmp.pdf')
+
+    ax = geneplot(chrom, start, end, figsize=(10, 5))
+    ax.plot(np.linspace(start, end, 1000), np.random.random(1000), 'o')
+    plt.savefig('tmp2.pdf')
