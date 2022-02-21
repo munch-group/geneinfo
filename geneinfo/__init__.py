8099fb33d93c335bf04bf46854dcc3a30bce3cb9
Kasper Munch <kaspermunch@birc.au.dk>
2022-02-21 11:54:04 +0100

    Improved layout and highlighting of genes

--- geneinfo/__init__.py
+++ geneinfo/__init__.py
@@ -1,8 +1,8 @@
 
-
 from IPython.display import Markdown, display
 import matplotlib.pyplot as plt
 import numpy as np
+from collections import defaultdict
 
 mg = None
 
@@ -77,7 +77,7 @@ def get_genes(chrom, start, end, hg19=False):
                 yield gene['symbol'], first_transcript['txstart'], first_transcript['txend'], first_transcript['strand'], first_transcript['position'], tophit['type_of_gene']
 
 
-def plot_gene(name, txstart, txend, strand, exons, gene_type, offset, ax, clip_on=True):
+def plot_gene(name, txstart, txend, strand, exons, gene_type, offset, line_width, font_size, ax, highlight=False, clip_on=True):
 
     if gene_type == 'protein-coding':
         if strand == 1:
@@ -92,8 +92,7 @@ def plot_gene(name, txstart, txend, strand, exons, gene_type, offset, ax, clip_o
     else:
         color='green'
 
-    
-    line = ax.plot([txstart, txend], [offset, offset], color=color)
+    line = ax.plot([txstart, txend], [offset, offset], color=color, linewidth=line_width/10)
 
     # arrowpoints = np.arange(txstart, txend, 5000)
     # y = np.full_like(arrowpoints, offset)
@@ -101,21 +100,24 @@ def plot_gene(name, txstart, txend, strand, exons, gene_type, offset, ax, clip_o
     # [add_arrow(arrowline, position=x) for x in arrowpoints[:-1]]
 
     for start, end in exons:
-        line = ax.plot([start, end], [offset, offset], linewidth=10, color=color)
+        line = ax.plot([start, end], [offset, offset], linewidth=line_width, color=color)
         line[0].set_solid_capstyle('butt')
         
-    ax.text(txstart, offset+.5, name, horizontalalignment='right', verticalalignment='center', fontsize=8, clip_on=clip_on)#, transform=ax.transAxes)
+    if highlight:
+        ax.text(txstart, offset+.5, name, horizontalalignment='right', verticalalignment='center', fontsize=font_size, weight='bold', color='red', clip_on=clip_on)#, transform=ax.transAxes)
+    else:
+        ax.text(txstart, offset+.5, name, horizontalalignment='right', verticalalignment='center', fontsize=font_size, clip_on=clip_on)#, transform=ax.transAxes)
 
 
 CACHE = dict()
 
-def geneplot(chrom, start, end, hg19=False, figsize=None, clip_on=True):
+def geneplot(chrom, start, end, highlight=[], hg19=False, figsize=None, clip_on=True):
     "Specifying hg19 gives gene coordintes in hg19, but give chrom, start and end are still assumed to be 38"
     
     global CACHE
 
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=figsize, sharex='col', sharey='row')
-    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.subplots_adjust(wspace=0, hspace=0.05)
 
     if (chrom, start, end, hg19) in CACHE:
         genes = CACHE[(chrom, start, end, hg19)]
@@ -123,13 +125,40 @@ def geneplot(chrom, start, end, hg19=False, figsize=None, clip_on=True):
         genes = list(get_genes(chrom, start, end, hg19=hg19))
         CACHE[(chrom, start, end, hg19)] = genes
 
-    offset = 0 # incase of no genes
+    label_width = (end - start) / 5
+        
+    plotted_intervals = defaultdict(list)
+    for name, txstart, txend, strand, exons, gene_type in genes:
+        gene_interval = [txstart-label_width, txend]
+        max_gene_rows = 200
+        for offset in range(1, max_gene_rows, 1):
+            if not intersect([gene_interval], plotted_intervals[offset]) and \
+                not intersect([gene_interval], plotted_intervals[offset-1]) and \
+                not intersect([gene_interval], plotted_intervals[offset+1]) and \
+                not intersect([gene_interval], plotted_intervals[offset-2]) and \
+                not intersect([gene_interval], plotted_intervals[offset+2]) and \
+                not intersect([gene_interval], plotted_intervals[offset-3]) and \
+                not intersect([gene_interval], plotted_intervals[offset+3]):
+                break
+        if plotted_intervals[offset]:
+            plotted_intervals[offset] = union(plotted_intervals[offset], [gene_interval])
+        else:
+            plotted_intervals[offset] = [gene_interval]
+    
+        from math import log10
+        line_width, font_size = min(12, int(12 / log10(len(genes))))-2, min(12, int(12 / log10(len(genes))))
+        plot_gene(name, txstart, txend, strand, exons, gene_type, 
+                  offset, line_width, font_size, 
+                  highlight=name in highlight,
+                  ax=ax2, clip_on=clip_on)
 
-    for offset, (name, txstart, txend, strand, exons, gene_type) in enumerate(genes):
-        plot_gene(name, txstart, txend, strand, exons, gene_type, offset, ax=ax2, clip_on=clip_on)
+    offset = max(plotted_intervals.keys())
 
-    ax2.get_yaxis().set_visible(False)
     ax2.set_ylim(-1, offset+3)
+    ax2.get_yaxis().set_visible(False)
+    ax2.spines['top'].set_visible(False)
+    ax2.spines['right'].set_visible(False)
+    ax2.spines['left'].set_visible(False)
 
     points = ax2.plot(txstart, offset+.5, 'o', ms=25, alpha=0, zorder=10)
 
