3f1113cb624cb9cfea7dce54a0f0dfd0c6541478
Kasper Munch <kaspermunch@birc.au.dk>
2024-10-23 18:05:44 +0200

    update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="geneinfo",
-    version="3.4.28",
+    version="3.4.29",
     author="Kasper Munch",
     author_email="kaspermunch@birc.au.dk",
     description="Functions for showing gene information in jupyter notebooks.",
