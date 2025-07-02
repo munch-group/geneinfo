120024b7eb663201a53475c971e5c884e7a84fb8
Kasper Munch <kaspermunch@birc.au.dk>
2025-07-02 16:11:35 +0200

    update

--- src/geneinfo/plot/venn.py
+++ src/geneinfo/plot/venn.py
@@ -380,6 +380,7 @@ def venn_dispatch(data, func, fmt="{size}", hint_hidden=False, cmap="Set2",
     _data = data.copy()
     for k, v in data.items():
         _data[k] = _data[k].intersection(set(background))
+        assert _data[k], f"Set {k} is empty after intersection with background"
         removed = len(data[k]) - len(_data[k])
         print(f'Ignoring {removed} genes in "{k}" not part of background set', file=sys.stderr)
     data = _data
